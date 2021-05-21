3f8af6a6b752734dbf228a944f1bc439a16cd8b4
gurpreet203181 <45843946+gurpreet203181@users.noreply.github.com>
2021-05-21 13:01:04 +0200

    Add project files.

---
+++ Souvenir_Classi/articolo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Souvenir_Classi
+{
+    public class Articolo
+    {
+        public string Name { get; set; }
+        public double Costo { get; set; }
+
+        public Articolo(string n, double c)
+        {
+            Name = n;
+            Costo = c;
+        }
+    }
+}
---
+++ Souvenir_Narzia/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Souvenir_Classi;
+
+namespace Souvenir_Narzia
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        List<Articolo> articoli = new List<Articolo>();
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Articolo a = new Articolo("Felpa_black", 19.99);
+            articoli.Add(a);
+            lst_articoli.Items.Add(a.Name);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Articolo a = new Articolo("Felpa_white", 19.99);
+            articoli.Add(a);
+            lst_articoli.Items.Add(a.Name);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Articolo a = new Articolo("Tshirt_black", 9.99);
+            articoli.Add(a);
+            lst_articoli.Items.Add(a.Name);
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Articolo a = new Articolo("Tshirt_white", 9.99);
+            articoli.Add(a);
+            lst_articoli.Items.Add(a.Name);
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            Articolo a = new Articolo("Cover_vaporwave", 5.99);
+            articoli.Add(a);
+            lst_articoli.Items.Add(a.Name);
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            new Souvenir_Narzia.Cassa().ShowDialog();
+            this.Close();
+        }
+    }
+}
