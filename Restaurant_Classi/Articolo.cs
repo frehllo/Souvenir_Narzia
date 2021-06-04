e9cb70e297e52e98314aaf2a320d8f3f63ab4a8d
gurpreet203181 <45843946+gurpreet203181@users.noreply.github.com>
2021-06-04 12:58:40 +0200

    finire restaurant

---
+++ Restaurant_Classi/Articolo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_Classi
+{
+    public class Articolo
+    {
+        public string Nome { get; set; }
+        public double Costo { get; set; }
+    }
+}
